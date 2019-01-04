3c07c87e98e8687a9d830c33ee9ecd69d74afa23
Archie Baer <archie@serveradmin.xyz>
2019-01-04 13:01:37 +0000

    Update vars.mjs

--- modules/vars.mjs
+++ modules/vars.mjs
@@ -1,5 +1,7 @@
 export default {
   "setvar" : function (params) {
     sulfur.vars[params[0]] = decodeURIComponent(params[1]);
+    //If is number, don't set as string
+    sulfur.vars[params[0]] = isNaN(sulfur.vars[params[0]]) ? sulfur.vars[params[0]] : Number(sulfur.vars[params[0]]);
   }
 }
