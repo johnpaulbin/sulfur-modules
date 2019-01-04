9ee51f8a2b1d7b0da78b021f8bfa67a9ee8cda31
Archie Baer <archie@serveradmin.xyz>
2019-01-04 19:06:37 +0000

    Update logic.mjs

--- modules/logic.mjs
+++ modules/logic.mjs
@@ -1,6 +1,6 @@
 export default {
   "if" : function (params) {
-    var conditionValue = sulfur.logic.condition(decodeURIComonent(sulfur.vars[params[0]]));
+    var conditionValue = sulfur.logic.condition(decodeURIComponent(sulfur.vars[params[0]]));
     //2nd Param is invert
     conditionValue = params[1] === "invert" ? !conditionValue : conditionValue;
     if (!conditionValue) {
