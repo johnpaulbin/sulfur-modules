e989bc784e1603170c72c76727ebb5f6563226f1
Archie Baer <archie@serveradmin.xyz>
2019-01-04 18:24:59 +0000

    Create logic.mjs

---
+++ modules/logic.mjs
@@ -0,0 +1,11 @@
+export default {
+  "if" : function (params) {
+    var conditionValue = sulfur.logic.condition(decodeURIComonent(sulfur.vars[params[0]]));
+    //2nd Param is invert
+    conditionValue = params[1] === "invert" ? !conditionValue : conditionValue;
+    if (!conditionValue) {
+      //Skip next line if not true
+      sulfur.lineNumber++;
+    }
+  }
+}
