112fcc09ae9e786c8ccf4c3ff12111fb764461c5
Archie Baer <archie@serveradmin.xyz>
2019-01-04 12:33:30 +0000

    Update linepos.mjs

--- modules/linepos.mjs
+++ modules/linepos.mjs
@@ -4,15 +4,5 @@ export default {
   },
   "go2point" : function (params) {
     sulfur.lineNumber = sulfur.vars["linepos_" + params[0]];
-  },
-  "go2line" : function (params) {
-    var newlinenum = Number(params[0]);
-    sulfur.lineLog(newlinenum);
-    if (isNaN(newlinenum)) {
-      sulfur.lineLog(params[0] + " is not a number. Skipping command.");
-      return;
-    }
-    sulfur.lineNumber = newlinenum;
-    sulfur.lineLog("Changed to line " + newlinenum - 1); //Always change to newlinenum - 1 as will increase 1 after command is run
   }
 }
