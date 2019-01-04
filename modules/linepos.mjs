1550a185f2b851bd9ab3215fe4c49375fd112550
Archie Baer <archie@serveradmin.xyz>
2019-01-04 18:45:57 +0000

    Update linepos.mjs

--- modules/linepos.mjs
+++ modules/linepos.mjs
@@ -4,5 +4,18 @@ export default {
   },
   "go2point" : function (params) {
     sulfur.lineNumber = sulfur.vars["linepos_" + params[0]];
+  },
+  "go2line" : function (params) {
+    var line = sulfur.num(params[0]);
+    if (line) {
+      if (line > 0 && line < sulfur.code.length) {
+        sulfur.lineNumber = line - 1; //Because increases after command
+        sulfur.lineLog("Moved to line " + line);
+      } else {
+        sulfur.lineLog(line + " is not a valid line");
+      }
+    } else {
+      sulfur.lineLog(params[0] + " is not a number");
+    }
   }
 }
