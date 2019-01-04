fa7ad8a66f042b656c1d8b965138e5fbf5baaa9d
Archie Baer <archie@serveradmin.xyz>
2019-01-04 18:42:29 +0000

    Update logic.mjs

--- modules/logic.mjs
+++ modules/logic.mjs
@@ -1,11 +1,21 @@
 export default {
-  "if" : function (params) {
-    var conditionValue = sulfur.logic.condition(decodeURIComonent(sulfur.vars[params[0]]));
-    //2nd Param is invert
-    conditionValue = params[1] === "invert" ? !conditionValue : conditionValue;
-    if (!conditionValue) {
-      //Skip next line if not true
-      sulfur.lineNumber++;
+  "setpoint" : function (params) {
+    sulfur.vars["linepos_" + params[0]] = sulfur.lineNumber;
+  },
+  "go2point" : function (params) {
+    sulfur.lineNumber = sulfur.vars["linepos_" + params[0]];
+  },
+  "go2line" : function (params) {
+    var line = sulfur.num(params[0]);
+    if (line) {
+      if (line > 0 && line < sulfur.code.length) {
+        sulfur.lineNumber = line - 1; //Because increases after command
+        sulfur.lineLog("Moved to line " + line);
+      } else {
+        sulfur.lineLog(line + " is not a valid line");
+      }
+    } else {
+      sulfur.lineLog(params[0] + " is not a number");
     }
   }
 }
